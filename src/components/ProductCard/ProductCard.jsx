ad0a17fe2e37fb20954633240e83bb929abf6490
fatih <fsenyigit8@gmail.com>
2024-03-22 23:44:13 +0300

    product structure

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import { categories } from '../../helper/data';
+import { products } from '../../helper/data';
 
 
 const ProductCard = () => {
   return (
-    <div>ProductCard</div>
+    <div className='container'>
+        {products.map((e)=>{
+            return (
+              <div className="product-container">
+                <img src={e.image} alt="" />
+                <h3>{e.price}</h3>
+                <p>{e.title}</p>
+              </div>
+            );
+        })}
+    </div>
   )
 }
 
